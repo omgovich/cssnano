8b2197cdf0b9466c839b69ee8005b9dbf0ad2547
Ludovico Fischer <livrerie@gmail.com>
2021-03-18 18:46:54 +0100

    docs: try to run cssnano 5 in the playground

--- site/src/components/editor/postcss_runner.js
+++ site/src/components/editor/postcss_runner.js
@@ -2,43 +2,32 @@
 /* eslint-disable no-unused-vars */
 import postcss from 'postcss';
 import { pkgnameToVarName } from '../../helper/naming';
+import cssnanoPresetLite from 'cssnano-preset-lite';
+import cssnanoPresetAdvanced from 'cssnano-preset-advanced';
+import cssnanoPresetDefault from 'cssnano-preset-default';
 
-/**
- * using moduleMaps and not with imports to lazy load them because of this error
- * editor lazy namespace object?f49d:5 Uncaught (in promise) Error: Cannot find module 'cssnano-preset-default'
-    at eval (eval at ./src/components/editor lazy recursive
- * need to fix this
- */
 const moduleMap = {
-  cssnanoPresetDefault: require('cssnano-preset-default'),
-  cssnanoPresetAdvanced: require('cssnano-preset-advanced'),
-  cssnanoPresetLite: require('cssnano-preset-lite'),
+  cssnanoPresetLite,
+  cssnanoPresetAdvanced,
+  cssnanoPresetDefault,
 };
 
-function initializePlugin(plugin, css, result) {
-  if (Array.isArray(plugin)) {
+export default (input, config) => {
+  const configName = pkgnameToVarName(config[0]);
+  const { plugins: nanoPlugins } = moduleMap[configName](config[1]);
+  const postcssPlugins = [];
+  for (const plugin of nanoPlugins) {
     const [processor, opts] = plugin;
     if (
-      typeof opts === 'undefined' ||
-      (typeof opts === 'object' && !opts.exclude)
-    ) {
-      return Promise.resolve(processor(opts)(css, result));
+        typeof opts === 'undefined' ||
+        (typeof opts === 'object' && !opts.exclude) ||
+        (typeof opts === 'boolean' && opts === true)
+      ) {
+        postcssPlugins.push(processor(opts));
     }
-  } else {
-    return Promise.resolve(plugin()(css, result));
   }
-  // Handle excluded plugins
-  return Promise.resolve();
-}
-
-export default (input, config) => {
-  const { plugins } = moduleMap[pkgnameToVarName(config[0])](config[1]);
-  const pluginRunner = (css, result) =>
-    plugins.reduce((promise, plugin) => {
-      return promise.then(initializePlugin.bind(null, plugin, css, result));
-    }, Promise.resolve());
   return new Promise((resolve, reject) => {
-    postcss(pluginRunner)
+    postcss(postcssPlugins)
       .process(input)
       .then((res) => resolve(res))
       .catch((err) => reject(err));
