95436bb78f119b5870a5f2a240158ea1ce6bade4
Ludovico Fischer <livrerie@gmail.com>
2021-04-22 21:19:37 +0200

    Deploy website - based on c49f4df0c1cfa1afc3269ca87abd27460ac1fbb3

---
+++ assets/js/7ceafc63.e94de72f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{99:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"cssnano 5 is available","permalink":"/blog/cssnano-5-release-notes"}]}')}}]);
